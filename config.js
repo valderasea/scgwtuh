1e92d9d7d83a5f64e8a86a5a31f7c191adc789b5
valderasea <157552843+valderasea@users.noreply.github.com>
2026-03-08 21:31:29 +0700

    Upload config.js from Bot

---
+++ config.js
@@ -0,0 +1,75 @@
+import { watchFile, unwatchFile } from 'fs';
+import chalk from 'chalk';
+import { fileURLToPath } from 'url';
+
+global.pairingNumber = 6289662866070;
+global.owner = [
+  ['6282249763982', 'ValL', false],['6289662866070', 'Val', true]];
+
+global.GH_TOKEN = 'TOKEN LU';
+global.GH_USER = 'USERNAME GH LU';
+
+global.namebot = 'ValL-Assistant';
+global.author = 'ValL';
+global.source = 'https://me-val.vercel.app';
+
+global.wait = 'Loading...';
+global.eror = 'Terjadi Kesalahan...';
+
+global.stickpack = 'Created By';
+global.stickauth = namebot;
+
+global.multiplier = 38; // The higher, The harder levelup
+
+/*============== EMOJI ==============*/
+global.rpg = {
+	emoticon(string) {
+		string = string.toLowerCase();
+		let emot = {
+			level: '📊',
+			limit: '🎫',
+			health: '❤️',
+			stamina: '🔋',
+			exp: '✨',
+			money: '💹',
+			bank: '🏦',
+			potion: '🥤',
+			diamond: '💎',
+			common: '📦',
+			uncommon: '🛍️',
+			mythic: '🎁',
+			legendary: '🗃️',
+			superior: '💼',
+			pet: '🔖',
+			trash: '🗑',
+			armor: '🥼',
+			sword: '⚔️',
+			pickaxe: '⛏️',
+			fishingrod: '🎣',
+			wood: '🪵',
+			rock: '🪨',
+			string: '🕸️',
+			horse: '🐴',
+			cat: '🐱',
+			dog: '🐶',
+			fox: '🦊',
+			petFood: '🍖',
+			iron: '⛓️',
+			gold: '🪙',
+			emerald: '❇️',
+			upgrader: '🧰',
+		};
+		let results = Object.keys(emot)
+			.map((v) => [v, new RegExp(v, 'gi')])
+			.filter((v) => v[1].test(string));
+		if (!results.length) return '';
+		else return emot[results[0][0]];
+	},
+};
+
+let file = fileURLToPath(import.meta.url);
+watchFile(file, () => {
+	unwatchFile(file);
+	console.log(chalk.redBright("Update 'config.js'"));
+	import(`${file}?update=${Date.now()}`);
+});
