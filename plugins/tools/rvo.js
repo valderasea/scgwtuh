822441d91d77443cc885df6e3194458a7b096aee
valderasea <157552843+valderasea@users.noreply.github.com>
2026-03-08 21:33:01 +0700

    Upload rvo.js from Bot

---
+++ plugins/tools/rvo.js
@@ -0,0 +1,17 @@
+let handler = async (m) => {
+	if (!m.quoted) return m.reply('Reply gambar/video yang ingin Anda lihat');
+	if (m.quoted.mediaMessage[m.quoted?.mediaType]?.viewOnce) {
+		let msg = await m.getQuotedObj()?.message;
+		let type = Object.keys(msg)[0];
+		let media = (await m.quoted?.download()) || (await m.getQuotedObj().download());
+		if (!media) return m.reply('Media gagal di Eksekusi!');
+
+		await conn.sendFile(m.chat, media, 'error.mp4', msg[type]?.caption || '', m);
+	} else m.reply('Ini bukan pesan view-once.');
+};
+
+handler.help = ['rvo'];
+handler.tags = ['tools'];
+handler.command = /^rvo|read/i;
+
+export default handler;
